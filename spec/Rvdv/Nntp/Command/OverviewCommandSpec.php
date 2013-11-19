4f10b44fa97060a8735404dc3236e7ab09426a8a
Robin van der Vleuten <robinvdvleuten@gmail.com>
2013-11-19 12:23:16 +0100

    Added the OverviewCommand and OverviewFormatCommand.

---
+++ spec/Rvdv/Nntp/Command/OverviewCommandSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\Rvdv\Nntp\Command;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class OverviewCommandSpec extends ObjectBehavior
+{
+    public function let()
+    {
+        $this->beConstructedWith('10-10', array());
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('Rvdv\Nntp\Command\OverviewCommand');
+    }
+
+    public function it_implements_interface()
+    {
+        $this->shouldImplement('Rvdv\Nntp\Command\CommandInterface');
+    }
+
+    public function it_should_be_a_multiline_command()
+    {
+        $this->isMultiLine()->shouldBe(true);
+    }
+
+    public function it_should_send_xover_command()
+    {
+        $this->execute()->shouldBe('XOVER 10-10');
+    }
+}
