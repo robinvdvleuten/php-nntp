1e87776221fa3ee96482cca61f0e63ed0a3e3e2f
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-01-22 09:58:25 +0100

    Added some docblock comments.

--- src/Rvdv/Nntp/Response/MultiLineResponse.php
+++ src/Rvdv/Nntp/Response/MultiLineResponse.php
@@ -2,6 +2,11 @@
 
 namespace Rvdv\Nntp\Response;
 
+/**
+ * MultiLineResponse
+ *
+ * @author Robin van der Vleuten <robinvdvleuten@gmail.com>
+ */
 class MultiLineResponse implements MultiLineResponseInterface
 {
     /**
@@ -14,22 +19,37 @@ class MultiLineResponse implements MultiLineResponseInterface
      */
     private $response;
 
+    /**
+     * Constructor
+     *
+     * @param Rvdv\Nntp\Response\ResponseInterface $response A ResponseInterface instance.
+     * @param ArrayAccess                          $lines    An ArrayAccess instance.
+     */
     public function __construct(ResponseInterface $response, \ArrayAccess $lines)
     {
         $this->response = $response;
         $this->lines = $lines;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getMessage()
     {
         return $this->response->getMessage();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getStatusCode()
     {
         return $this->response->getStatusCode();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getLines()
     {
         return $this->lines;
