75e9b73973e4f4d9b616a9b7415501883b0ec39a
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-03-03 11:30:06 +0100

    Test if an error is thrown when connection can't be established.

--- tests/Rvdv/Nntp/Tests/Connection/ConnectionTest.php
+++ tests/Rvdv/Nntp/Tests/Connection/ConnectionTest.php
@@ -20,4 +20,16 @@ class ConnectionTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('server ready - posting allowed', $response->getMessage());
     }
+
+    public function testErrorIsThrownWhenConnectionCannotBeEstablished()
+    {
+        $connection = new Connection('unknownhost', 3000);
+
+        try {
+            $connection->connect();
+            $this->fail('->connect() throws a Rvdv\Nntp\Exception\RuntimeException because the connection cannot be established');
+        } catch (\Exception $e) {
+            $this->assertInstanceof('Rvdv\Nntp\Exception\RuntimeException', $e, '->connect() throws a Rvdv\Nntp\Exception\RuntimeException because the connection cannot be established');
+        }
+    }
 }
