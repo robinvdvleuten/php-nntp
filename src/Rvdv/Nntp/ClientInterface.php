5f0105cbf86f87973d43e2d50f107d5528faba18
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-03-03 11:17:54 +0100

    Added some docblock annotations to the client interface.

--- src/Rvdv/Nntp/ClientInterface.php
+++ src/Rvdv/Nntp/ClientInterface.php
@@ -12,28 +12,76 @@ interface ClientInterface
     /**
      * Establish a connection with the NNTP server.
      *
-     * @return Rvdv\Nntp\Response\ResponseInterface A response containing the welcome message of the server.
+     * @return \Rvdv\Nntp\Response\ResponseInterface A response containing the welcome message of the server.
      */
-    function connect();
+    public function connect();
 
     /**
      * Disconnect the connection with the NNTP server.
      *
      * @return bool A boolean indicating if the connection is disconnected.
      */
-    function disconnect();
+    public function disconnect();
 
-    function authInfo($type, $value);
+    /**
+     * Send the AUTHINFO command.
+     *
+     * @param $type
+     * @param $value
+     *
+     * @return \Rvdv\Nntp\Command\AuthInfoCommand
+     */
+    public function authInfo($type, $value);
 
-    function group($name);
+    /**
+     * Send the GROUP command.
+     *
+     * @param $name
+     *
+     * @return \Rvdv\Nntp\Command\GroupCommand
+     */
+    public function group($name);
 
-    function overview($from, $to, array $format);
+    /**
+     * Send the XOVER command.
+     *
+     * @param $from
+     * @param $to
+     * @param array $format
+     *
+     * @return \Rvdv\Nntp\Command\OverviewCommand
+     */
+    public function overview($from, $to, array $format);
 
-    function overviewFormat();
+    /**
+     * Send the LIST OVERVIEW.FMT command.
+     *
+     * @return \Rvdv\Nntp\Command\OverviewFormatCommand
+     */
+    public function overviewFormat();
 
-    function quit();
+    /**
+     * Send the QUIT command.
+     *
+     * @return \Rvdv\Nntp\Command\QuitCommand
+     */
+    public function quit();
 
-    function xfeature($feature);
+    /**
+     * Send the XFEATURE command.
+     *
+     * @param $feature
+     *
+     * @return \Rvdv\Nntp\Command\XFeatureCommand
+     */
+    public function xfeature($feature);
 
-    function sendCommand(CommandInterface $command);
+    /**
+     * Send the given command to the NNTP server.
+     *
+     * @param \Rvdv\Nntp\Command\CommandInterface $command
+     *
+     * @return \Rvdv\Nntp\Command\CommandInterface
+     */
+    public function sendCommand(CommandInterface $command);
 }
