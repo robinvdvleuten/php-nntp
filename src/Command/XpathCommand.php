e7b4afc69e1c87deba7a21e933de9b07287529da
Maxime Fabre <ehtnam6@gmail.com>
2015-10-29 11:15:25 +0100

    Add XPATH command

---
+++ src/Command/XpathCommand.php
@@ -0,0 +1,71 @@
+<?php
+namespace Rvdv\Nntp\Command;
+
+use Rvdv\Nntp\Response\Response;
+
+class XpathCommand extends Command implements CommandInterface
+{
+    /**
+     * @var int
+     */
+    const FOUND_PATH = 223;
+
+    /**
+     * @var int
+     */
+    const INVALID_REFERENCE = 501;
+
+    /**
+     * @var string
+     */
+    private $reference;
+
+    /**
+     * Constructor.
+     *
+     * @param string $reference The reference
+     */
+    public function __construct($reference)
+    {
+        $this->reference = $reference;
+
+        parent::__construct([]);
+    }
+
+    /**
+     * @return string
+     */
+    public function execute()
+    {
+        return sprintf('XPATH %s', $this->reference);
+    }
+
+    /**
+     * @return array
+     */
+    public function getExpectedResponseCodes()
+    {
+        return [
+            self::FOUND_PATH        => 'onFoundPath',
+            self::INVALID_REFERENCE => 'onInvalidMessage',
+        ];
+    }
+
+    /**
+     * Return the message's reference
+     *
+     * @param Response $response
+     */
+    public function onFoundPath(Response $response)
+    {
+        $this->result = $response->getMessage();
+    }
+
+    /**
+     * If we didn't find the message, just return an empty response
+     */
+    public function onInvalidMessage()
+    {
+        $this->result = null;
+    }
+}
