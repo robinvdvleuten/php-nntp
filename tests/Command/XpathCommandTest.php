fad90391c925ddf4fecc0222b0f02c7cd3f439f1
Robin van der Vleuten <robinvdvleuten@gmail.com>
2015-10-29 14:26:43 +0100

    Fixed CS issues

--- tests/Command/XpathCommandTest.php
+++ tests/Command/XpathCommandTest.php
@@ -1,11 +1,20 @@
 <?php
+
+/*
+ * This file is part of the NNTP library.
+ *
+ * (c) Robin van der Vleuten <robinvdvleuten@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rvdv\Nntp\Tests\Command;
 
 use Rvdv\Nntp\Command\XpathCommand;
 
 class XpathCommandTest extends CommandTest
 {
-
     public function testItReturnsStringWhenExecuting()
     {
         $command = $this->createCommandInstance();
