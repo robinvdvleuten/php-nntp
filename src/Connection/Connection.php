478675f0a7660091550f0f662e42ee2de366c8bc
Philippe Villiers <kissifrot@gmail.com>
2016-11-02 15:03:13 +0100

    Fix getMultiLineResponse mangling data
    Response is now correctly fetched, especially with yEnc encoded responses

--- src/Connection/Connection.php
+++ src/Connection/Connection.php
@@ -186,30 +186,33 @@ class Connection implements ConnectionInterface
 
     public function getMultiLineResponse(Response $response)
     {
-        $buffer = '';
+        $lines = [];
 
         while (!$this->socket->eof()) {
-            $buffer .= $this->socket->gets(self::BUFFER_SIZE);
-
-            if ("\n.\r\n" === substr($buffer, -4)) {
-                break;
+            $line = $this->socket->gets(self::BUFFER_SIZE);
+            if (substr($line, -2) !== "\r\n" || strlen($line) < 2) {
+                continue;
             }
 
-            if ($buffer === false) {
-                $this->disconnect();
-                throw new RuntimeException('Incorrect data received from buffer');
+            // Remove CR LF from the end of the line.
+            $line = substr($line, 0, -2);
+
+            // Check if the line terminates the text response.
+            if ($line === '.') {
+                // Return all previous lines.
+                $lines = array_filter($lines);
+                $lines = \SplFixedArray::fromArray($lines);
+                return new MultiLineResponse($response, $lines);
             }
+
+            // If 1st char is '.' it's doubled (NNTP/RFC977 2.4.1).
+            if (substr($line, 0, 2) === '..') {
+                $line = substr($line, 1);
+            }
+
+            // Add the line to the array of lines.
+            $lines[] = $line;
         }
-
-        $lines = explode("\r\n", trim($buffer));
-        if (end($lines) === '.') {
-            array_pop($lines);
-        }
-
-        $lines = array_filter($lines);
-        $lines = \SplFixedArray::fromArray($lines);
-
-        return new MultiLineResponse($response, $lines);
     }
 
     public function getCompressedResponse(Response $response)
